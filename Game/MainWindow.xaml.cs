c55dc482383675fb83b6fb82ccc001511dec24a5
HermanGorba <hermanhorba2@gmail.com>
2024-06-12 23:46:11 +0300

    Добавьте файлы проекта.

---
+++ Game/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sudoku sudoku;
+        TextBox[,] textBoxes;
+        int unguessed;
+        int incorrectAnswers;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GenerateSudokuGrid();
+            NewGameButton.Focus();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = (sender as TextBox)?.Text.Length == 1 || !int.TryParse(e.Text, out _);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                int row = Grid.GetRow(textBox);
+                int col = Grid.GetColumn(textBox);
+
+                // because of separators
+                int solutionRow = row > 2 ? (row > 6 ? row - 2 : row - 1) : row;
+                int solutionCol = col > 2 ? (col > 6 ? col - 2 : col - 1) : col;
+
+                if (int.TryParse(textBox.Text, out int num) && sudoku.Solution[solutionRow, solutionCol] == num)
+                {
+                    ToGuessedTextBoxStyle(textBox);
+
+                    if (sudoku.Grid[solutionRow, solutionCol] == 0)
+                    {
+                        textBox.Background = Brushes.White;
+                        textBox.Foreground = Brushes.Blue;
+                        unguessed--;
+
+                        if (unguessed == 0)
+                        {
+                            SolutionButton.IsEnabled = false;
+                            NewGameButton.IsEnabled = true;
+
+                            if (IsSolvedByUser())
+                                MessageBox.Show($"You won with {incorrectAnswers} incorrect answers. Well played!", "Sudoku solved!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                            else
+                                MessageBox.Show($"Game ended with {incorrectAnswers} incorrect answers.\nDon\'t get upset!", $"Sudoku solved!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                        }
+                    }
+                } 
+                else if (textBox.Text == string.Empty)
+                {
+                    ResetTextBoxStyle(textBox);
+                }
+                else 
+                {
+                    ToIncorrectTextBoxStyle(textBox);
+                    incorrectAnswers++;
+                }
+
+            }
+        }
+
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            incorrectAnswers = 0;
+            unguessed = 40;
+            sudoku = new Sudoku();
+
+            sudoku.Generate(unguessed);
+            SolutionButton.IsEnabled = true;
+            NewGameButton.IsEnabled = false;
+            
+            foreach (var textBox in textBoxes)
+                ResetTextBoxStyle(textBox);
+            
+
+            for (int i = 0; i < Sudoku.GridSize; i++) 
+            {
+                for (int j = 0; j < Sudoku.GridSize; j++) 
+                {
+                    TextBox textBox = textBoxes[i, j];
+                    int num = sudoku.Grid[i, j];
+
+                    if (num > 0)
+                        textBox.Text = num.ToString();
+                }
+            }
+        }
+        private void ShowSolutionButton_Click(object sender, RoutedEventArgs e)
+        {
+            SolutionButton.IsEnabled = false;
+            NewGameButton.IsEnabled = true;
+
+            for (int i = 0; i < Sudoku.GridSize; i++)
+            {
+                for (int j = 0; j < Sudoku.GridSize; j++)
+                {
+                    TextBox textBox = textBoxes[i, j];
+
+                    if (textBox.IsEnabled == true)
+                    {
+                        textBox.Text = sudoku.Solution[i, j].ToString();
+                        ToSolvedTextBoxStyle(textBox);
+                    }
+                }
+            }
+        }
+
+
+        private void GenerateSudokuGrid()
+        {
+            int gridSize = Sudoku.GridSize;
+            textBoxes = new TextBox[gridSize, gridSize];
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    // because of separators
+                    int currentRow = row > 2 ? (row > 5 ? row + 2 : row + 1) : row;
+                    int currentCol = col > 2 ? (col > 5 ? col + 2 : col + 1) : col;
+
+                    TextBox textBox = new()
+                    {
+                        IsEnabled = false,
+                        HorizontalContentAlignment = HorizontalAlignment.Center,
+                        VerticalContentAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        FontSize = this.Width / 20,
+                        BorderBrush = new SolidColorBrush(Color.FromRgb(242, 242, 242)),
+                        BorderThickness = new Thickness(3)
+                        
+                    };
+                    textBox.TextChanged += TextBox_TextChanged;
+                    textBox.PreviewTextInput += TextBox_PreviewTextInput;
+                    textBoxes[row, col] = textBox;
+
+                    Grid.SetRow(textBox, currentRow);
+                    Grid.SetColumn(textBox, currentCol);
+
+                    SudokuGrid.Children.Add(textBox);
+                }
+            }
+        }
+        private bool IsSolvedByUser() 
+        {
+            for (int i = 0; i < textBoxes.GetLength(0); i++)
+                for (int j = 0; j < textBoxes.GetLength(1); j++)
+                    if (textBoxes[i, j].Foreground == Brushes.Red)
+                        return false;
+            return true;
+        }
+        private static void ToGuessedTextBoxStyle(in TextBox textBox)
+        {
+            textBox.IsEnabled = false;
+            textBox.FontWeight = FontWeights.Bold;
+        }
+        private static void ResetTextBoxStyle(in TextBox textBox)
+        {
+            textBox.Text = string.Empty;
+            textBox.IsEnabled = true;
+            textBox.FontWeight = FontWeights.Normal;
+            textBox.Foreground = Brushes.Black;
+            textBox.Background = Brushes.White;
+        }
+        private static void ToSolvedTextBoxStyle(in TextBox textBox)
+        {
+            textBox.IsEnabled = false;
+            textBox.FontWeight = FontWeights.Bold;
+            textBox.Foreground = Brushes.Red;
+        }
+        private static void ToIncorrectTextBoxStyle(in TextBox textBox) 
+        {
+            textBox.Background = Brushes.LightCoral;
+        }
+    }
+}
---
+++ Game/Sudoku.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Sudoku
+    {
+        private const int gridSize = 9;
+        private const int subGridSize = 3;
+        private readonly int[,] grid = new int[GridSize, GridSize];
+        private int[,] solution = new int[GridSize, GridSize];
+        private static readonly int[] numbers;
+
+        private Random rand = new Random();
+
+
+
+
+        public static int GridSize => gridSize;
+        public int[,] Grid => grid;
+        public int[,] Solution => solution;
+        
+
+        static Sudoku()
+        {
+            numbers = new int[GridSize];
+            for (int i = 1; i <= GridSize; i++)
+                numbers[i - 1] = i;
+        }
+
+        public async void Generate(int toRemove)
+        {
+            await Solve(0, 0);
+            solution = (int[,])grid.Clone();
+            RemoveNumbers(toRemove);
+        }
+
+        private int[] Rearrange(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                int j = i + rand.Next(length - i);
+                (array[i], array[j]) = (array[j], array[i]);
+            }
+            return array;
+        }
+
+        private async Task<bool> Solve(int row, int col)
+        {
+            if (row == GridSize)
+                return true;
+
+            if (col == GridSize)
+                return await Solve(row + 1, 0);
+
+
+            foreach (int num in Rearrange(numbers))
+            {
+                if (IsValidMove(row, col, num))
+                {
+                    grid[row, col] = num;
+
+                    if (await Solve(row, col + 1))
+                        return true;
+
+                    grid[row, col] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsValidMove(int row, int col, int num)
+        {
+            return !UsedInRow(row, num) && !UsedInCol(col, num) && !UsedInSubgrid(row - row % subGridSize, col - col % subGridSize, num);
+        }
+
+        private bool UsedInRow(int row, int num)
+        {
+            for (int col = 0; col < GridSize; col++)
+                if (grid[row, col] == num)
+                    return true;
+            return false;
+        }
+
+        private bool UsedInCol(int col, int num)
+        {
+            for (int row = 0; row < GridSize; row++)
+                if (grid[row, col] == num)
+                    return true;
+            return false;
+        }
+
+        private bool UsedInSubgrid(int row, int col, int num)
+        {
+            for (int i = 0; i < subGridSize; i++)
+                for (int j = 0; j < subGridSize; j++)
+                    if (grid[i + row, j + col] == num)
+                        return true;
+            return false;
+        }
+
+        private void RemoveNumbers(int toRemove)
+        {
+            while (toRemove > 0)
+            {
+                int row = rand.Next(GridSize);
+                int col = rand.Next(GridSize);
+                if (grid[row, col] != 0)
+                {
+                    grid[row, col] = 0;
+                    toRemove--;
+                }
+            }
+        }
+    }
+}
